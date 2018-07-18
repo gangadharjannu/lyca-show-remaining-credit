5ec96a9e802aff02c1705d5e88a7103a42f845d5
gangadharjannu <gangadhar014@gmail.com>
2018-07-18 21:39:25 +0200

    Added constants.js for selectors

---
+++ constants.js
@@ -0,0 +1,23 @@
+// dom element selectors
+
+module.exports = {
+    // selector
+    SELECTOR: {
+        GDPR_NOTICE_REMOVE: '#CybotCookiebotDialogBodyButtonAccept',
+        USERNAME: '#ctl00_cphPro_txtMobileNumber',
+        PASSWORD: '#ctl00_cphPro_txtPassword',
+        LOGIN_BUTTON: '#ctl00_cphPro_btnLogin',
+
+        CALL_HISTORY_MONTH_SELECT: '#ctl00_cphPro_ddlListPeriod',
+        CALL_HISTORY_MONTH_SUBMIT: '#ctl00_cphPro_btnSubmit'
+    },
+    // page
+    PAGE: {
+        LOGIN: 'https://account.lycamobile.nl/login.aspx?lang=en',
+        ACCOUNT_INFO: 'https://account.lycamobile.nl/Account/AccountInfo.aspx?lang=en',
+        CALL_HISTORY: 'https://account.lycamobile.nl/topup/cdrreport.aspx?lang=en'
+    },
+
+
+
+}
